a694e7d31b58ca87db02b9dd22212a815c13d66f
Toron Seigler <toronseigler9@gmail.com>
2019-08-19 19:31:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,13 @@
-end
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
   end
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end
+
+end
